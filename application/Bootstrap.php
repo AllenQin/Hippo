304c00fa9a342ff747ab9c6495b3d73b066de68f
秦海龙 <qinhailong@zmengzhu.com>
2019-01-07 16:30:34 +0800

    更新Library

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -5,50 +5,26 @@ use Yaf\Dispatcher;
 
 class Bootstrap extends Bootstrap_Abstract
 {
-    /**
-     * 初始化一些配置信息
-     */
     public function _initConfig()
     {
     }
 
-    /**
-     * 初始化Loader
-     */
     public function _initLoader()
     {
-
     }
 
-    /**
-     * 初始化依赖注入services
-     */
     public function _initServices()
     {
-
     }
 
-    /**
-     * 初始化全局事件监听
-     */
     public function _initListener()
     {
     }
 
-    /**
-     * 插件初始化
-     *
-     * @param Dispatcher $dispatcher
-     */
     public function _initPlugin(Dispatcher $dispatcher)
     {
     }
 
-    /**
-     * 初始化视图
-     *
-     * @param Dispatcher $dispatcher
-     */
     public function _initView(Dispatcher $dispatcher)
     {
         // 不自动渲染视图
