ac10fc346ddd6611069774c83eb6a22e4c400a04
秦海龙 <allenqinhai@gmail.com>
2019-02-24 14:27:42 +0800

    fix welcome page

---
+++ application/modules/Web/controllers/Index.php
@@ -0,0 +1,11 @@
+<?php
+
+use App\Library\Core\MVC\Controller;
+
+class IndexController extends Controller
+{
+    public function indexAction()
+    {
+        $this->display('index', ['content' => 'Hello Hippo!']);
+    }
+}
